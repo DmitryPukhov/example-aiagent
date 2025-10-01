f798d50cee3ab78e532258e3042eae023b04e571
dmitriy.pukhov <pukhov@gnivc.ru>
2025-10-01 16:21:41 +0300

    Updated to load app/index by default

--- aiagent-gui/app/_layout.tsx
+++ aiagent-gui/app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+
+//  // This hook will set the document title when the screen comes into focus
+//   useFocusEffect(() => {
+//     // This is the standard web API to set the browser tab title
+//     if (typeof document !== 'undefined') {
+//       document.title = "Mecanicien";
+//     }
+//   });
+
+  return (<Stack >
+      <Stack.Screen name="index"
+          options={{
+          title: "Mecanicien", // Set your desired title here,
+        }}  />    
+    </Stack>);
 }
